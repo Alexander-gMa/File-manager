c2cfc5c235f33cc7f4a05aa4354ccdb55374a219
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-06-07 23:48:32 +0300

    feat: add logic for user exit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import readline from 'readline';
+import { homedir } from 'os';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -6,17 +7,40 @@ const rl = readline.createInterface({
 });
 
 function startManager(args = process.argv) {
+    console.log(homedir());
     if (args.length < 3) {
         console.log('Please enter user name (--username=your_name)');
+        rl.close();
         return;
     }
     const userInfo = args[2].split('=');
     let [userKey, userName] = userInfo;
+
+    process.openStdin().on("keypress", (chunk, key) => {
+        if(key && key.name === "c" && key.ctrl) {
+          process.exit(userName);
+        }
+      });
+    process.on('exit', (name) => {
+        console.info(`Thank you for using File Manager, ${name}!`);
+        rl.close();
+    })
+
     if (userKey.startsWith('--')) userKey = userKey.slice(2);
     if (userKey === 'username') {
         console.log(`Welcome to the File Manager, ${userName}!`);
     } else {
         console.log('Please enter user name (--username=your_name)');
     }
+
+    rl.on('line', (input) => {
+        const [operation, ...path] = input.split(' ');
+
+        switch (operation) {
+            case '.exit': {
+                process.exit(userName);
+            }
+        }
+    });
 }
 startManager();
